947a741aef9b68e6a911b19cbfba27ffc934d3cb
sivakrishna <42761102+sivakrishna-b@users.noreply.github.com>
2018-11-22 12:36:43 +0530

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,9 +1,9 @@
 s=int(input())
-b=[int(x) for x in input().split()]
-b.sort()
+c=[int(x) for x in input().split()]
+c.sort()
 for i in range(0,s):
 		if(i<s-1):
 				k=' '
 		else:
 				k=''
-		print(b[i],end=k)
+		print(c[i],end=k)
