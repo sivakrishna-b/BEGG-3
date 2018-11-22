fc5924d20c607cfb4c422ed04c5c5a0f305dd003
sivakrishna <42761102+sivakrishna-b@users.noreply.github.com>
2018-11-22 12:26:39 +0530

    Update 24.py

--- 24.py
+++ 24.py
@@ -2,7 +2,7 @@ number=int(input("enter the length:"))
 list=[]
 for i in range(0,number):
     newNo=int(input(''))
-    a=list.append(newNo)
+    b=list.append(newNo)
     print list
 list.sort()
 print("Sorted list is",list)
