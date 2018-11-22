8123418c51d8da8fd63676a96a062dafe1f4ebf9
sivakrishna <42761102+sivakrishna-b@users.noreply.github.com>
2018-11-22 12:35:44 +0530

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,8 +1,9 @@
-number=int(input("enter the length:"))
-list=[]
-for i in range(0,number):
-    newNo=int(input(''))
-    b=list.append(newNo)
-    print list
-list.sort()
-print("Sorted list is",list)
+s=int(input())
+b=[int(x) for x in input().split()]
+b.sort()
+for i in range(0,s):
+		if(i<s-1):
+				k=' '
+		else:
+				k=''
+		print(b[i],end=k)
